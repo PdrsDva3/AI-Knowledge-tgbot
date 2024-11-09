62712c060da687aaf00a80d2dd8d092983807ed8
PdrsDv3 <denis.kalinkin.05@gmail.com>
2024-11-09 17:57:39 +0300

    deploy commit

--- config.template.py
+++ config.template.py
@@ -1,14 +1,16 @@
 from aiogram import Bot, Dispatcher, Router
 
-TOKEN_TG = "your token"
-DB_NAME = "postgres"
-DB_PORT = 5432
-DB_PASSWORD = "postgres"
-DB_USER = "postgres"
-DB_HOST = "localhost"
+TOKEN_TG = "token"
 
-NoneData = ""
 # Инициализация бота
 bot = Bot(token=TOKEN_TG)
 dp = Dispatcher()
 router = Router()
+
+DB_NAME = "postgres"
+DB_PORT = 5432
+DB_PASSWORD = "postgres"
+DB_USER = "postgres"
+DB_HOST = "postgres"
+
+NoneData = ""
